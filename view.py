5dc042ac9fb4c234b563f6c6426b6a4c3f485082
Colin <colin.baumgard@gmail.com>
2019-04-13 17:45:03 +0200

    interface graphique

--- view.py
+++ view.py
@@ -1,5 +1,17 @@
+import mainwindow
 
+from PyQt5 import QtCore, QtGui, QtWidgets
+import sys
 
 class View:
     def __init__(self):
-        pass
+        self.init_window()
+
+
+    def init_window(self):
+        app = QtWidgets.QApplication(sys.argv)
+        principale_ihm = QtWidgets.QMainWindow()
+        ui = mainwindow.Ui_MainWindow()
+        ui.setupUi(principale_ihm)
+        principale_ihm.show()
+        sys.exit(app.exec_())
